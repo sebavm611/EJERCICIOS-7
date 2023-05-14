0eed2f46e4eb3fb11a9f91540d2d3fcd6403fe84
PABLO VELEZ <pablo.velez@es.uazuay.edu.ec>
2023-05-13 22:05:49 -0500

    modificacion de archivos

--- Ejercicio5/Ejercicio4/src/NumerosPerfectos.java
+++ Ejercicio5/Ejercicio4/src/NumerosPerfectos.java
@@ -1,14 +1,8 @@
 
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author adm
+ * @author Rodrigo Ortiz y Pablo Velez
  */
 public class NumerosPerfectos {
 
@@ -88,3 +82,4 @@ public class NumerosPerfectos {
         }
     }
 }
+

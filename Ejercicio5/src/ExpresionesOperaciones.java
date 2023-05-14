e23765e86e84aaec324f878d9fe96a45968f61bf
sebavm611 <115278126+sebavm611@users.noreply.github.com>
2023-05-13 22:00:51 -0500

    Update ExpresionesOperaciones.java

--- Ejercicio5/src/ExpresionesOperaciones.java
+++ Ejercicio5/src/ExpresionesOperaciones.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author adm
+ * @author Rodrigo Ortiz y Pablo Velez
  */
 import java.util.Scanner;
 
